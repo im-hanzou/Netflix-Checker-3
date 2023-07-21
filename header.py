4746c980c72ba04dc682416a80625a9e80a09660
Zeeckt <139068412+Zeeckt@users.noreply.github.com>
2023-07-20 23:30:50 -0300

    Add files via upload

---
+++ header.py
@@ -0,0 +1,19 @@
+def title():
+	print("\x1bc")
+	print("\n\033[38;5;52m███╗   ██╗███████╗████████╗███████╗██╗     ██╗██╗  ██╗")
+	print("\033[38;5;88m████╗  ██║██╔════╝╚══██╔══╝██╔════╝██║     ██║╚██╗██╔╝")
+	print("\033[38;5;124m██╔██╗ ██║█████╗     ██║   █████╗  ██║     ██║ ╚███╔╝ ")
+	print("\033[38;5;160m██║╚██╗██║██╔══╝     ██║   ██╔══╝  ██║     ██║ ██╔██╗ ")
+	print("\033[38;5;196m██║ ╚████║███████╗   ██║   ██║     ███████╗██║██╔╝ ██╗")
+	print("\033[38;5;160m╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝     ╚══════╝╚═╝╚═╝  ╚═╝")
+	print("\033[38;5;124m    =========== https://github.com/g3th =========    ")
+
+def user_options():
+	print("\n\033[38;5;7m--------- Options: ---------\n")
+	print("1) Check Accounts")
+	print("2) Check Countries and Stats in Combo-List")
+	print("3) Eliminate Countries from List (check accounts with any VPN)\n")
+
+	
+			
+		
---
+++ start.bat
@@ -0,0 +1,3 @@
+cls
+python netflix.py
+pause
